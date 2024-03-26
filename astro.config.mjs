581cae0d7b44761d8999f58a912e6bc99d0f3c32
Mapetr <info@mapetr.moe>
2024-03-26 19:15:59 +0100

    add optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), sitemap(), compress(), cloudflare()],
   markdown: {
     syntaxHighlight: "prism"
-  }
+  },
+  prefetch: {
+    defaultStrategy: "hover"
+  },
+  output: "server",
+  adapter: cloudflare()
 });
