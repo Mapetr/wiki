d14f63f33a535cea2af490a596e97a933cd6ea4d
Mapetr <info@mapetr.moe>
2024-03-26 22:32:12 +0100

    add cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig, passthroughImageService} from 'astro/config';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 import cloudflare from "@astrojs/cloudflare";
+import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), sitemap(), compress(), cloudflare()],
+  integrations: [mdx(), tailwind(), sitemap(), compress()],
   markdown: {
     syntaxHighlight: "prism"
   },
   prefetch: {
     defaultStrategy: "hover"
   },
+  image: {
+    provider: passthroughImageService()
+  },
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  site: "https://wiki.ayai.dev"
 });
